792c952ddd338962336914a3964ce59f31209841
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-10-18 23:01:22 +0500

    add streamlit to pick image

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st 
+
+# Project Title
+st.title("Room Classification Project")
+
+# Team Member
+st.write("""
+         House & Apartaments Classification model
+         #### TEAM MEMBER
+         - Сидоркин Георгий Владимирович РИМ-130908
+         - Рахарди Сандикха РИМ-130908
+         -
+         -
+         
+         #### Our Project
+         """)
+
+# Upload the image sam
+uploadedFile = st.file_uploader('Upload image here')
+
+if uploadedFile is not None:
+    st.write("Filename:", uploadedFile.name)
+    st.write("Data:", uploadedFile)
+    
+    
