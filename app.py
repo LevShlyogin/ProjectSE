17e5878f924e8a5b8c857682bd309fe6e46c4825
soulvi <147710292+soulvi@users.noreply.github.com>
2024-05-28 22:51:24 +0500

    upd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 from transformers import pipeline
-
+from typing import Optional, Dict
 
 def load_image():
     uploadedFile = st.file_uploader('Upload image here')
@@ -51,3 +51,20 @@ if result:
     prediction = model.predict(loadedImage)
     st.write("""#### Output""")
     st.write(prediction)
+
+def display_project_title(title: str):
+    """Display the project title.
+
+    Args:
+        title (str): The title of the project.
+    """
+    st.title(title)
+
+
+def display_team_info(team_info: str):
+    """Display the information about the team.
+
+    Args:
+        team_info (str): The markdown string containing team information.
+    """
+    st.write(team_info)
